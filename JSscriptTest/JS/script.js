41ecc8e20fe21eaa0215847bc48b4f5419e35442
Swiety <sy.bereza@gmail.com>
2023-07-02 10:23:43 +0200

    firts commit

---
+++ JSscriptTest/JS/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert("Hello!");
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Old +18?", "18");
+//console.log(answer + 5);
+
+const category = "toys";
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Hi, ${user}`);
